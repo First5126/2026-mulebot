b5966ec9615acd75a774f7cafd68ba4b1c711c6c
Mark-Lua <s62711@parkhill.k12.mo.us>
2026-01-21 17:43:05 -0600

    Added back trust and auto command paths

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.constants.AprilTagLocalizationConstants;
+import frc.robot.constants.WaypointConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.vision.AprilTagLocalization;
@@ -43,7 +44,6 @@ public class RobotContainer {
 
     private final CommandXboxController joystick = new CommandXboxController(0);
 
-
     public RobotContainer() {
         configureBindings();
     }
@@ -53,7 +53,7 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             drivetrain.gasPedalCommand(joystick::getRightTriggerAxis, joystick::getLeftTriggerAxis, joystick::getRightX, joystick::getLeftY, joystick::getLeftX));
-
+        
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
@@ -63,16 +63,8 @@ public class RobotContainer {
 
         joystick.a().onTrue(m_aprilTagLocalization.setTrust(true));
         joystick.a().onFalse(m_aprilTagLocalization.setTrust(false));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.start().onTrue(WaypointConstants.goNearDepot);
 
         // Reset the field-centric heading on left bumper press.
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
---
+++ src/main/java/frc/robot/constants/WaypointConstants.java
@@ -0,0 +1,24 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.generated.TunerConstants;
+
+public class WaypointConstants {
+    // TODO: the acceleration is defently off so we need to find out the true value
+    private static final PathConstraints constraints = new PathConstraints(
+        DrivetrainConstants.maxSpeedMetersPerSecond, DrivetrainConstants.maxSpeedMetersPerSecond,
+        DrivetrainConstants.maxAngularVelocityRadiansPerSecond, DrivetrainConstants.maxAngularVelocityRadiansPerSecond);
+
+    private static final Pose2d nearDepotPose = new Pose2d(2,6,Rotation2d.fromDegrees(0));
+
+    public static final Command goNearDepot = AutoBuilder.pathfindToPose(nearDepotPose,constraints);
+
+}

3b320e79235ba6d5e440c489f8134e7ba7179d0c
SDPowerStrok3 <cole.howdeshell@gmail.com>
2026-01-26 14:56:54 -0600

    Removed Frame Limiting

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -4,19 +4,20 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CameraConstants;
 import frc.robot.vision.PhotonVisionHelpers;
 
-public class PhotonVision implements Subsystem {
+public class PhotonVision extends SubsystemBase {
 
     public PhotonCamera camera;
 
     public PhotonVision() {
         camera = CameraConstants.camera1;
-        camera.setFPSLimit(30);
     }
 
     public void Periodic() {
+        System.out.println(camera.isConnected());
         PhotonPipelineResult result = PhotonVisionHelpers.getResultOfCamera(camera);
         if (result.hasTargets()) {
             System.out.println(result.getBestTarget());

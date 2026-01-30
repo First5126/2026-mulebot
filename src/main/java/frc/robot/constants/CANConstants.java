767397347ec93cad60edcad0300cb2275826ecf9
Brick219 <s86434@parkhill.k12.mo.us>
2026-01-30 17:43:29 -0600

    Added Turret subsystem and CANConstands

---
+++ src/main/java/frc/robot/constants/CANConstants.java
@@ -0,0 +1,23 @@
+package frc.robot.constants;
+
+
+public class CANConstants{
+
+    public static final int frontLeftDrive = 2;
+    public static final int frontRightDrive = 8;
+    public static final int backLeftDrive = 6;
+    public static final int backRightDrive = 9;
+
+    public static final int frontLeftRotation = 1;
+    public static final int frontRightRotation = 3;
+    public static final int backLeftRotation = 5;
+    public static final int backRightRotation = 10;
+
+    public static final int swerveFrontLeft = 2;
+    public static final int swerveFrontRight = 3;
+    public static final int swerveBackLeft = 0;
+    public static final int swerveBackRight = 4;
+
+    public static final int turretEncoder = 10;
+    public static final int turretMotor = 11;
+}

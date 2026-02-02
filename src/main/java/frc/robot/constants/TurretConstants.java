92d8776f60faf128e795bd161dc0bc649ef9077b
Greg Howdeshell <greg.howdeshell@gmail.com>
2026-02-01 18:21:58 -0600

    Update turret positions to a min/max configuration

--- src/main/java/frc/robot/constants/TurretConstants.java
+++ src/main/java/frc/robot/constants/TurretConstants.java
@@ -1,7 +1,10 @@
 package frc.robot.constants;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
+
 public class TurretConstants {
-        
-
-
+    public static final Angle MIN_ANGLE = Units.Degrees.of(0);
+    public static final Angle MAX_ANGLE = Units.Degrees.of(360);
 }

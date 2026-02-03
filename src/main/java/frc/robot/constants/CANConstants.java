7ed7b0c37af14e5d7eb986c6f97fa8bdbdd31e65
RoWiz09 <robotwizard09@gmail.com>
2026-02-02 19:25:50 -0600

    Hood Subsystem (#9)

--- src/main/java/frc/robot/constants/CANConstants.java
+++ src/main/java/frc/robot/constants/CANConstants.java
@@ -22,5 +22,7 @@ public class CANConstants{
     public static final int turretEncoder = 10;
     public static final int turretMotor = 11;
 
+    public static final int hoodMotor = 0; // TODO: Set the real id once we have it
+
     public static CANBus driveBaseCanivore =  new CANBus("DriveBase");
 }

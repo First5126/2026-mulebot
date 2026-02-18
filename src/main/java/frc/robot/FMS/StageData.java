e7143fc7dc451ba59f63b29de6d5ec45b097c203
Greg Howdeshell <greg.howdeshell@gmail.com>
2026-02-18 16:20:54 -0600

    Update the usage of StageData to make more readable

--- src/main/java/frc/robot/FMS/StageData.java
+++ src/main/java/frc/robot/FMS/StageData.java
@@ -5,45 +5,68 @@
 package frc.robot.FMS;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.Optional;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Timer;
 
-public class StageData extends SubsystemBase {
-  private static char m_firstInactiveAlliance;
-  private static char m_alliance;
-
-  public StageData() {
-    m_firstInactiveAlliance = ' ';
-    m_alliance = ' ';
-  }
-
-  @Override
-  public void periodic() {
-    if (m_alliance == ' ') {
-      Optional<DriverStation.Alliance> our_alliance = DriverStation.getAlliance();
-      if (our_alliance.isPresent()) {
-        m_alliance = our_alliance.get().name().charAt(0);
-      }
-    }
-
-    if (m_firstInactiveAlliance == ' ') {
-      String gameData = DriverStation.getGameSpecificMessage();
-      if (gameData.length() > 0) {
-        m_firstInactiveAlliance = gameData.charAt(0);
-      }
-    }
-  }
-
+/** Class for managing the stages the robot can be within during the match */
+public class StageData {
+  /*
+   * Enumeration to keep track of the shifts including the time in each shift.  Do note the computation
+   * of the seconds utilized in this enum are based on a running clock time and not a remaining clock time.
+   * It simplifies the usage of the data.
+   */
   public enum GameStage {
-    Auto,
-    TransitionShift,
-    ShiftOne,
-    ShiftTwo,
-    ShiftThree,
-    ShiftFour,
-    Endgame
+    Auto(20),
+    TransitionShift(10),
+    ShiftOne(25),
+    ShiftTwo(25),
+    ShiftThree(25),
+    ShiftFour(25),
+    Endgame(30);
+
+    private final int m_secondsInStage;
+    private final int m_stageOverAtSeconds;
+
+    GameStage(int secondsInStage) {
+      this.m_secondsInStage = secondsInStage;
+      this.m_stageOverAtSeconds = computeStageOverAtSeconds(this);
+    }
+
+    private static int computeStageOverAtSeconds(GameStage current) {
+      int total = 0;
+      for (GameStage s : GameStage.values()) {
+        total += s.m_secondsInStage;
+        if (s == current) {
+          return total;
+        }
+      }
+      throw new IllegalStateException("Stage not found: " + current);
+    }
+
+    /**
+     * Get the seconds that are in the current stage
+     *
+     * @return {@int} an integer value representing the seconds that exist for the stage
+     */
+    public int getSecondsInStage() {
+      return m_secondsInStage;
+    }
+
+    /**
+     * Get the seconds at which the stage will be over
+     *
+     * @return {@int} an integer indicating at what seconds into the match will the stage be over
+     */
+    public int getStageOverAtSeconds() {
+      return m_stageOverAtSeconds;
+    }
   }
 
+  /**
+   * Get the current stage in the match.
+   *
+   * @return {@GameStage} stage related to the current gameplay
+   */
   public static GameStage getStage() {
     boolean auto = DriverStation.isAutonomous();
     if (auto) return GameStage.Auto;
@@ -57,16 +80,73 @@ public class StageData extends SubsystemBase {
     else return GameStage.TransitionShift;
   }
 
-  public static boolean canScore() {
+  /**
+   * @return int remaining seconds for the current stage
+   */
+  public int getRemainingSecondsInStage() {
+    if (Timer.getMatchTime() == -1.0) {
+      return 0;
+    } else {
+      return StageData.getStage().getStageOverAtSeconds() - (150 - (int) Timer.getMatchTime());
+    }
+  }
+
+  /**
+   * Returns if the robot can currently score on the hub. This takes into account who is the first
+   * active alliance and determing if you can store on 1/3 shifts or 2/4 shifts.
+   *
+   * @return {@boolean} returns {true} if the robot can score on the hub; otherwise, {false} is
+   *     returned
+   */
+  public boolean canScore() {
     GameStage stage = getStage();
 
-    if (stage == GameStage.Auto || stage == GameStage.Endgame) return true;
-    else if (stage == GameStage.ShiftOne || stage == GameStage.ShiftThree) {
-      if (m_alliance == m_firstInactiveAlliance) return false;
-      else return true;
-    } else if (stage == GameStage.ShiftTwo || stage == GameStage.ShiftFour) {
-      if (m_alliance == m_firstInactiveAlliance) return true;
-      else return false;
-    } else return false;
+    if (stage == GameStage.Auto || stage == GameStage.Endgame || stage == GameStage.TransitionShift)
+      return true;
+
+    // if my current alliance is the first to be active, I can shoot in shift 1 and 3
+    if (getCurentAlliance() == StageData.getFirstActiveAlliance()) {
+      switch (stage) {
+        case ShiftOne:
+        case ShiftThree:
+          return true;
+        default:
+          return false;
+      }
+
+    } else {
+      // I can shoot in shift 2 and 4
+      switch (stage) {
+        case ShiftTwo:
+        case ShiftFour:
+          return true;
+        default:
+          return false;
+      }
+    }
+  }
+
+  // helper for getting the current alliance
+  private DriverStation.Alliance getCurentAlliance() {
+    return DriverStation.getAlliance().orElse(null);
+  }
+
+  // return who is the first active alliance based on the game data.  if no game data is present
+  // the first aliance is determined by the current aliance selected in driverstation.  until
+  // logic is further vetted, the default may reverse but until utilized, we'll wait to see.
+  private static Alliance getFirstActiveAlliance() {
+    String message = DriverStation.getGameSpecificMessage();
+    if (message.length() > 0) {
+      // capture the first inactive alliance
+      char cVal = message.charAt(0);
+      if (cVal == 'R') {
+        return Alliance.Blue;
+      } else {
+        return Alliance.Red;
+      }
+    }
+    return DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue
+        ? Alliance.Red
+        : Alliance.Blue;
   }
 }
